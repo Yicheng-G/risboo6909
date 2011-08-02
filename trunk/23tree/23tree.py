90a52150d3dac63b607c2caf40d65267017638fc
ttyv00@gmail.com <ttyv00@gmail.com@24185b1e-5e1d-f923-bac4-94118aae4d90>
2011-08-02 23:10:09 +0000

    fixes

--- trunk/23tree/23tree.py
+++ trunk/23tree/23tree.py
@@ -51,6 +51,9 @@ class Value(object):
 
 class Node(object):
 
+    LEFT = 0
+    RIGHT = 1
+
     def __init__(self, v = None, parent = None):
         self.__values = []
         self.__parent = parent
@@ -66,6 +69,21 @@ class Node(object):
         for v in self.values:
             out += (str(v) + ' ')
         return out
+
+    def __getlink(self, a):
+        if self.valcnt == 1:
+            if a < self.min:    return self.values[0], self.LEFT
+            if a > self.max:    return self.values[0], self.RIGHT
+        elif self.valcnt == 2:
+            if a < self.min:    return self.values[0], self.LEFT
+            if a > self.max:    return self.values[1], self.RIGHT
+            if a > self.min and a < self.max: return self.values[0], self.RIGHT
+        elif self.valcnt == 3:
+            if a < self.min:    return self.values[0], self.LEFT
+            if a > self.max:    return self.values[2], self.RIGHT
+            if a > self.min and a < self.med:   return self.values[1], self.LEFT
+            if a > self.med and a < self.max:   return self.values[1], self.RIGHT
+        return None, None
 
     # interface methods & properties
 
@@ -85,8 +103,10 @@ class Node(object):
     def removeLink(self, node):
         """ Remove link from self to node """
         for ref in self.values:
-            if self.lessThan == node: self.lessThan = None
-            if self.greaterThan == node: self.greaterThan = None
+            if ref.lessThan == node: 
+                ref.lessThan = None
+            if ref.greaterThan == node:
+                ref.greaterThan = None
 
     def isConsistent(self):
         """ Check whether the node is consistent, this means it doesn't contain 3 items or 4 links """
@@ -97,9 +117,13 @@ class Node(object):
         return self.refcnt == 0
 
     def addLink(self, nodeRef):
-        """ Add a link to another node """
-        if self.chooseChild(nodeRef):
-            self.chooseChild(nodeRef) = nodeRef
+        """ Add link to another node """
+        if nodeRef is None: return
+        ref, side = self.__getlink(nodeRef.min)
+        if ref != None:
+            if side == self.LEFT: ref.lessThan = nodeRef
+            else: ref.greaterThan = nodeRef
+            nodeRef.parent = self
 
     def contains(self, a):
         """ Check if node contains a given value """
@@ -108,19 +132,10 @@ class Node(object):
 
     def chooseChild(self, a):
         """ Choose where to go according to the value a """
-        if self.valcnt == 1:
-            if a < self.min:    return self.min.lessThan
-            if a > self.max:    return self.max.greaterThan
-        elif self.valcnt == 2:
-            if a < self.min:    return self.min.lessThan
-            if a > self.max:    return self.max.greaterThan
-            if a > self.min and a < self.max: return self.min.greaterThan
-        elif self.valcnt == 3:
-            if a < self.min:    return self.min.lessThan
-            if a > self.max:    return self.max.greaterThan
-            if a > self.min and a < self.med:   return self.med.lessThan
-            if a > self.med and a < self.max:   return self.med.greaterThan
-        return None
+        ref, side = self.__getlink(a)
+        if ref != None:
+            if side == self.LEFT: return ref.lessThan
+            else: return ref.greaterThan
 
     @property
     def min(self):
@@ -144,11 +159,10 @@ class Node(object):
     def refcnt(self):
         tmp = set()
         for val in self.values:
-            tmp.add(val.lessThan)
-            tmp.add(val.greaterThan)
-        if len(tmp) == 1 and tmp[0] == None:
-            # one 'None' link means that there are no links at all
-            return 0
+            if val.lessThan != None:
+                tmp.add(val.lessThan)
+            if val.greaterThan != None:
+                tmp.add(val.greaterThan)
         return len(tmp)
 
     @property
@@ -189,41 +203,40 @@ class TTTree(object):
         nextNode = curNode.chooseChild(a)
         if nextNode is None:
             return curNode
-        self.__find(nextNode, a)
+        return self.__find(nextNode, a)
 
     def __fixNode(self, node):
         if not node.isConsistent():
             # conflict detected, try to resolve it
             if node is self.root:
                 # take the middle element of a root node and treat it as a new root node
-                node.min.resetLinks()
-                node.max.resetLinks()
-                node.med.resetLinks()
-                mid = node.med
-                self.root = Node(mid)
-                mid.lessThan = Node(node.min, self.root)
-                mid.greaterThan = Node(node.max, self.root)
+                self.root = Node(node.med.value)
+                leftNode = Node(node.min.value, self.root)
+                rightNode = Node(node.max.value, self.root)
+                self.root.addLink(leftNode)
+                self.root.addLink(rightNode)
+
+                leftNode.addLink(node.med.lessThan)
+                rightNode.addLink(node.med.greaterThan)
             elif node.isLeafNode():
                 # case for leaf node
-                mid = node.med
-                # create two separate nodes
-                leftNode = Node(node.min)
-                rightNode = Node(node.max)
-
-                node.parent.removeLink(self)
-                node.parent.insertValue(mid)
+                print node.min.value, node.med.value, node.max.value
+                print node.parent
+                node.parent.insertValue(node.med.value)
+                leftNode = Node(node.min.value, node.parent)
+                rightNode = Node(node.max.value, node.parent)
+                node.parent.removeLink(node)
                 node.parent.addLink(leftNode)
                 node.parent.addLink(rightNode)
-
-                self.__fixNode(parent)
+                self.__fixNode(node.parent)
             else:
                 # case for internal node (the hardest one)
                 mid = node.med
 
-                leftNode = Node(node.min)
-                rightNode = Node(node.max)
+                leftNode = Node(node.min.value, node.parent)
+                rightNode = Node(node.max.value, node.parent)
 
-                node.parent.removeLink(self)
+                node.parent.removeLink(node)
                 
                 leftNode.addLink(mid.lessThan)
                 rightNode.addLink(mid.greaterThan)
@@ -231,7 +244,7 @@ class TTTree(object):
                 node.parent.addLink(leftNode)
                 node.parent.addLink(rightNode)
 
-                self.__fixNode(parent)
+                self.__fixNode(node.parent)
  
 
         else: return
@@ -248,11 +261,11 @@ class TTTree(object):
     def insertValue(self, a):
         """ Inserts a new value to tree and resolves conflicts if needed """
         node = self.findNode(a)
-        if node.contains(a): return None
+        if node.contains(a): 
+            return None
         # try to insert a new value into existing node
         node.insertValue(a)
         self.__fixNode(node)
-
         return node
         
     @property
@@ -268,6 +281,13 @@ t = TTTree()
 t.insertValue(12)
 t.insertValue(20)
 t.insertValue(15)
-print t.root.min.greaterThan
+
+t.insertValue(25)
+t.insertValue(27)
+t.insertValue(30)
+
+t.insertValue(26)
+
+print t.root.max.greaterThan.min.greaterThan
 
 
