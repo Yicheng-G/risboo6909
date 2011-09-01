958368fec872a37fbabf3f4fb7fcaefc352ccf15
ttyv00@gmail.com <ttyv00@gmail.com@24185b1e-5e1d-f923-bac4-94118aae4d90>
2011-09-01 18:29:50 +0000

    unittest optimized

--- trunk/23tree/unittests.py
+++ trunk/23tree/unittests.py
@@ -5,45 +5,53 @@
 from tttree import TTTree
 import random, time
 
-ITEMS = 1000000
-RANDMAX = 100000000000
-tree = TTTree(use_poll = True)
+ITEMS = 100000
+RANDMAX = 1000000
+
 
 def timer(f):
     def inner(*args, **kwargs):
         start_t = time.time()
         f(*args, **kwargs)
         print 'done in %f sec' % (time.time() - start_t)
+        return time.time() - start_t
     return inner
 
 @timer
-def insertTest(items):    
-    print 'test 1 - insert %d elements...' % ITEMS
-    net, elements = 0, 0
-    for j in xrange(ITEMS):
-        val = random.randint(0, RANDMAX)
-        if val not in items:
-            items.add(val)
-            a = time.time()
-            tree.insertValue(val)
-            net += (time.time() - a)
-            elements += 1
-    print 'tree insertion time %f, elements actually inserted %d' % (net, elements)
+def insertTest(tree, items):
+    print 'test 1 - insert %d elements...' % len(items)
+    for j in items:
+        tree.insertValue(j)
 
 @timer
-def consTest(items):  
+def consTest(tree, items): 
     print 'test 2 - checking consistency of %d elements...' % len(items)
     for j in items:
-        assert(tree.contains(j))       
+        assert(tree.contains(j)) 
 
 @timer
-def removeTest(items):
+def removeTest(tree, items):
     print 'test 3 - deleting all the elements...'
     for j in items:
-        tree.removeValue(j)     
+        tree.removeValue(j)
 
-items = set()
-insertTest(items)
-consTest(items)
-removeTest(items)
+ins, cons, rem  = [], [], []
+
+for x in xrange(10):
+    print 'Test N%d' % x
+    tree = TTTree()
+    print 'generating set...'
+    items = set()
+    for x in xrange(ITEMS):
+        val = random.randint(0, RANDMAX)
+        if val not in items: items.add(val)
+    print 'done!'
+    ins.append(insertTest(tree, items))
+    cons.append(consTest(tree, items))
+    rem.append(removeTest(tree, items))
+
+print
+print 'avg insert time %f' % (sum(ins) / len(ins))
+print 'avg cons time %f' % (sum(cons) / len(cons))
+print 'avg remove time %f' % (sum(rem) / len(rem))
 
