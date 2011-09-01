e5671e02606f59e744d0f9b7e09e9046eef39622
ttyv00 <ttyv00@gmail.com>
2011-09-01 12:12:55 +0000

    time measurement for tests

--- 23tree/unittests.py
+++ 23tree/unittests.py
@@ -3,28 +3,45 @@
 from tttree import TTTree
 import random, time
 
-ITEMS = 10
-RANDMAX = 100
+ITEMS = 1000000
+RANDMAX = 100000000000
 tree = TTTree()
 
-# test 1: random items insertion
-start_t = time.time()
-print 'test 1 - insert %d elements...' % ITEMS
+def timer(f):
+    def inner(*args, **kwargs):
+        start_t = time.time()
+        f(*args, **kwargs)
+        print 'done in %f sec' % (time.time() - start_t)
+    return inner
+
+@timer
+def insertTest(items):    
+    print 'test 1 - insert %d elements...' % ITEMS
+    net, elements = 0, 0
+    for j in xrange(ITEMS):
+        val = random.randint(0, RANDMAX)
+        if val not in items:
+            items.add(val)
+            a = time.time()
+            tree.insertValue(val)
+            net += (time.time() - a)
+            elements += 1
+    print 'tree insertion time %f, elements actually inserted %d' % (net, elements)
+
+@timer
+def consTest(items):  
+    print 'test 2 - checking consistency of %d elements...' % len(items)
+    for j in items:
+        assert(tree.contains(j))       
+
+@timer
+def removeTest(items):
+    print 'test 3 - deleting all the elements...'
+    for j in items:
+        tree.removeValue(j)     
+
 items = set()
-for j in xrange(ITEMS):
-    val = random.randint(0, RANDMAX) 
-    items.add(val)
-    tree.insertValue(val)
-print 'done in %f sec' % (time.time() - start_t)
+insertTest(items)
+consTest(items)
+removeTest(items)
 
-print 'test 2 - checking consistency...'
-for j in items:
-    assert(tree.contains(j))       
-print 'done!'
-
-print 'test 3 - deleting all the elements...'
-print items
-for j in items:
-    print j
-    tree.removeValue(j)     
-print 'done!'
