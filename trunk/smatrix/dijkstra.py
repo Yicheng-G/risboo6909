91db10c0693f6df8b4c34b753c7d68a3425bb547
ttyv00@gmail.com <ttyv00@gmail.com@24185b1e-5e1d-f923-bac4-94118aae4d90>
2011-09-24 21:57:04 +0000

    dijkstra works

--- trunk/smatrix/dijkstra.py
+++ trunk/smatrix/dijkstra.py
@@ -2,29 +2,31 @@
 # to avoid surplus memory usage in many cases
 
 from smatrix import SparseMatrix
+import heapq
 
-INF = 10 ** 5
+INF = 2 ** 31
 
 def getNeighbours(M, pointIdx):
-    tmp = []
+    tmp = []    
     for i in xrange(M.size()[1]):
-        if M[pointIdx][i] != -1:           
-            tmp.append((M[pointIdx][i], i))
-    tmp = sorted(tmp, key = lambda x: x[0], reverse = True)
+        if M[pointIdx][i] != -1 and i != pointIdx:
+            heapq.heappush(tmp, (M[pointIdx][i], i))
     return tmp
 
-def recursiveStep(lst, visited, M):
+def recursiveStep(cur_node, lst, visited, M):
+    minNode, minWeight, cur_weight = None, 0, M[cur_node][cur_node] 
     while lst:
-        item = lst.pop()
+        item = heapq.heappop(lst)
         weight, node = item
         if node in visited:
             continue
-        if M[node][node] is INF or M[node][node] > weight:
-            old_weight = 0
-            if M[node][node] is not INF:
-                old_weight = M[node][node]
-            M.setitem([node, node], old_weight + weight)
-            recursiveStep(getNeighbours(M, node), visited + [node], M)
+        if not minNode: 
+            minNode = node
+        if M[node][node] > cur_weight + weight:
+            M.setitem([node, node], cur_weight + weight)
+    if minNode:
+        visited.append(minNode)
+        recursiveStep(minNode, getNeighbours(M, minNode), visited, M)
 
 def dijkstraAlgorithm(M, startPoint):
     # make all the nodes weights equal to infinity first
@@ -35,13 +37,15 @@ def dijkstraAlgorithm(M, startPoint):
     # fill diagonal with INFs (initial distances)
     for i in xrange(rows):
         M.setitem([i, i], INF)
+    M.setitem([startPoint, startPoint], 0)
     # from - is a column of a matrix, to - is a row of a matrix
-    recursiveStep(getNeighbours(M, startPoint), [startPoint], M)
+    recursiveStep(startPoint, getNeighbours(M, startPoint), [startPoint], M)
 
+"""
 # test case
 M = SparseMatrix(dim = [6, 6], defaultVal = -1)
 
-# grpah has been taken from ru.wikipedia.org
+# graph has been taken from ru.wikipedia.org
 M.setitem([0, 5], 14)
 M.setitem([5, 0], 14)
 M.setitem([0, 2], 9)
@@ -58,10 +62,11 @@ M.setitem([2, 3], 11)
 M.setitem([3, 2], 11)
 M.setitem([1, 3], 15)
 M.setitem([3, 1], 15)
-M.setitem([4, 2], 6)
-M.setitem([2, 4], 6)
+M.setitem([4, 3], 6)
+M.setitem([3, 4], 6)
 
 dijkstraAlgorithm(M, 0)
 
 print M
+"""
 
