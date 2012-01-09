da7ffc94d9bfc881ad8fb02228ed2b76bfeffbfe
ttyv00 <ttyv00@gmail.com>
2012-01-09 16:01:48 +0000

    it works in parallel

--- entropy/core/engine.py
+++ entropy/core/engine.py
@@ -3,12 +3,16 @@
 from gawrapper import GAWrapper, Population
 from funclib import *
 
+def test(chromosome):
+    return sum(chromosome)
+
 engine = GAWrapper()
 
 # first of all - create and init a new population
 pop1 = Population()
 pop1.setFuncList([inc, dec])
 pop1.setMaxSpecies(20)
+pop1.setScoreF(test)
 
 # add population to engine
 engine.addPopulation(pop1)
