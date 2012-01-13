a0635a5ff24c2febff856b6c942fb95be1cc278e
ttyv00@gmail.com <ttyv00@gmail.com@24185b1e-5e1d-f923-bac4-94118aae4d90>
2012-01-13 18:02:00 +0000

    test example

---
+++ trunk/entropy/test1.py
@@ -0,0 +1,33 @@
+""" Main engine module """
+
+from core.gawrapper import GAWrapper, Population
+from core.funclib import *
+
+def score(alg):
+    pts = 50
+    try:
+        for x in xrange(0, 50):
+            if alg._eval([x, x]) == (x + x): pts -= 1
+    except:
+        pass
+    return pts
+
+engine = GAWrapper()
+
+# first of all - create and init a new population
+pop1 = Population()
+pop1.setFuncList([inc, dec, add, sub, mul, ident])
+pop1.setArgsReq(1)
+pop1.setMaxSpecies(30)
+pop1.setMaxAlgSize(3)
+pop1.setReportRate(100)
+pop1.setMaxGen(-1)
+pop1.setScoreF(score)
+pop1.setFileName('pop1.dat')
+
+# add population to engine
+engine.addPopulation(pop1)
+
+# start evolution!
+engine.startAll()
+
