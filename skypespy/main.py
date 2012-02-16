f4c817082c633c332864b057d2cdb0adcc0a8e4b
ttyv00 <ttyv00@gmail.com>
2012-02-16 12:31:29 +0000

    initial

---
+++ skypespy/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import Skype4Py
+import logging
+
+skype = Skype4Py.Skype() 
+
+def isSkypeRunning():
+    if not skype.Client.IsRunning:
+        logging.info('Skype is not running, trying to start...')
+        skype.Client.Start()
+    else:
+        logging.info('Skype is active, ok')
+
+def attachToSkype():
+    skype.FriendlyName = 'SkypeSpy'
+    skype.Attach()
+
+if __name__ == '__main__':
+    logging.basicConfig(filename = 'skypespy.log', format = '%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s', level = logging.INFO)
+    isSkypeRunning()
+    attachToSkype()
+    print skype.CurrentUser.FullName
