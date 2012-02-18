76f42d675499635f872a3a343ae3a3ce816400c7
ttyv00 <ttyv00@24185b1e-5e1d-f923-bac4-94118aae4d90>
2012-02-18 19:19:06 +0000

    working version with logging

--- trunk/skypespy/main.py
+++ trunk/skypespy/main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 from userinfo import UserInfo
+from readydata import ReadyData
+from aggr import aggregate
 
 import Skype4Py
 import logging, time
+import logging.handlers
 
 skype = Skype4Py.Skype(Transport='x11') 
-userdata = {}
+readyData = ReadyData(save_after = 4)
 
-REFRESH_DELAY = 10
+REFRESH_DELAY = 2
+DATA_DIR = 'data'
 
 def isSkypeRunning():
     if not skype.Client.IsRunning:
@@ -24,28 +28,50 @@ def attachToSkype():
 def refreshFriends():
     try:
         for friend in skype.Friends:
-            uinfo = userdata.get(friend.Handle, UserInfo())
+            uinfo = readyData.getUser(friend.Handle)
             uinfo.update(friend)
 #            print friend.Handle, friend.FullName, friend.Country, friend.OnlineStatus
     except:
         pass
 
-if __name__ == '__main__':
-    logging.basicConfig(filename = 'skypespy.log', format = '%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s',
-                             level = logging.INFO)
+def createLogger():
+    logger = logging.getLogger('skypespy')
+    logger.setLevel(logging.INFO)
+    rfh = logging.handlers.RotatingFileHandler('skypespy.log', maxBytes = 100000, backupCount = 2)
+    rfh.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s')
+    rfh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(rfh)
+    logger.addHandler(ch)
+    return logger
 
+if __name__ == '__main__':
+
+
+    logger = createLogger()
 
     isSkypeRunning()
     attachToSkype()
 
     while True:
-        try:
-            refreshFriends()
-            timestamp = time.ctime(time.time())
-            logging.info('Refreshed friends list at %s for user %s' % (timestamp, skype.CurrentUser.FullName))
-            print 'Refreshed friends list at %s for user %s' % (timestamp, skype.CurrentUser.FullName)
-            time.sleep(REFRESH_DELAY)
-        except KeyboardInterrupt:
-            print 'break'
-            break
 
+        try:
+
+            refreshFriends()
+
+            readyData.dumpData()
+            aggregate(DATA_DIR)
+
+            timestamp = time.ctime(time.time())
+
+            logger.info('Refreshed friends list at %s for user %s' % (timestamp, skype.CurrentUser.FullName))
+
+            time.sleep(REFRESH_DELAY)
+
+        except KeyboardInterrupt:
+            readyData.dumpData(force = True)
+            aggregate(DATA_DIR)
+            break
--- trunk/skypespy/userinfo.py
+++ trunk/skypespy/userinfo.py
@@ -6,28 +6,36 @@ class UserInfo(object):
         self.data = []
         self.update(user)
  
+    def getData(self):
+        return self.data
+
+    def setData(self, new_data):
+        self.data = new_data
+
     def diff(self, userattr):
         # check if there are any differences with previous check
         if len(self.data):
-            prev_data = self.data[-2][1]
+            prev_data = self.data[-1][1]
             isDifferent = False
             for prop in prev_data:
                 if prev_data[prop] != userattr[prop]:
+                    print 'diff', prev_data[prop], userattr[prop]
                     isDifferent = True
                     break
             return isDifferent
+        return True
 
     def update(self, user):
         if user:
+
             userattr = {
                 'skypename': user.Handle,
                 'fullname': user.FullName,
                 'country': user.Country,
                 'status': user.OnlineStatus,
                 'homepage': user.Homepage,
-                'msgsToMe': 0,
-                'msgsFromMe': 0,
             }
+
             if self.diff(userattr):
-                self.data.append( (time.time(), userattr,) )
+                self.data.append( (int(time.time()), userattr, ) )
 
